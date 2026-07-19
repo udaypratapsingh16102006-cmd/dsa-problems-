ba3c5f3861f2802baf9f82e39a31120a7a7c460f
udayyyy <udaypratapsingh16102006@gmail.com>
2026-07-20 00:09:33 +0530

    Time: 0 ms (100%), Space: 10.1 MB (30.18%) - LeetHub

--- 0078-subsets/0078-subsets.cpp
+++ 0078-subsets/0078-subsets.cpp
@@ -1,22 +1,20 @@
 class Solution {
 public:
-void findsub(int ind,int sum,vector<int>&arr,vector<vector<int>>&ans,int n,vector<int>&ds){
-    if(ind==n){
-    ans.push_back(ds);
-    return;    
+    vector<vector<int>> ans;
+    vector<int> subset;
+
+    void solve(vector<int>& nums, int index) {
+        ans.push_back(subset);
+
+        for (int i = index; i < nums.size(); i++) {
+            subset.push_back(nums[i]);   // Choose
+            solve(nums, i + 1);          // Explore
+            subset.pop_back();           // Backtrack
+        }
     }
-    ds.push_back(arr[ind]);//taking left recur
-    findsub(ind+1,sum+=arr[ind],arr,ans,n,ds);
-    ds.pop_back();
-    //if not taking
-    findsub(ind+1,sum,arr,ans,n,ds); 
-}
+
     vector<vector<int>> subsets(vector<int>& nums) {
-        int n=nums.size();
-        vector<vector<int>>ans;
-        vector<int>ds;
-        findsub(0,0,nums,ans,n,ds);
+        solve(nums, 0);
         return ans;
-        
     }
 };
